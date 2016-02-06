3eac39a8ae4cbc20e91850f912f49fc312a4766a
Georg Brandl <georg@python.org>
2016-02-06 18:30:39 +0100

    Nicer (and safer) implementation of the PIF commands.

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -162,13 +162,15 @@ impl Si {
         for i in 0..16 {
             ram[ram_start + i] = BigEndian::read_u32(&self.pif_ram[4*i..]);
         }
-        // println!("PIF read:\n{:#?}", &ram[ram_start..ram_start+16]);
+        // println!("\nPIF read:");
+        // for i in 0..8 {
+        //     println!("  {:#10x} {:#10x}", ram[ram_start+2*i], ram[ram_start+2*i+1]);
+        // }
     }
 
     fn dma_write(&mut self, ram: &[u32], cstate: u32) {
         // transfer 64 bytes main memory -> PIF ram
         let ram_start = self.reg_dram_addr as usize / 4;
-        // println!("PIF write:\n{:#?}", &ram[ram_start..ram_start+16]);
         for i in 0..16 {
             BigEndian::write_u32(&mut self.pif_ram[4*i..], ram[ram_start + i]);
         }
@@ -177,43 +179,59 @@ impl Si {
 
     fn execute(&mut self, cstate: u32) {
         // nothing to do?
-        if self.pif_ram[15] & 1 == 0 { return; }
+        if self.pif_ram[63] & 1 == 0 { return; }
         let mut channel = 0;
-        let mut byte = 0;
-        while byte < 63 {
-            byte += 1;
-            let ntrans = self.pif_ram[byte - 1];
+        let mut start = 0;
+        while start <= 60 {  // last possible byte for a command
+            let ntrans = self.pif_ram[start] as usize;
             if ntrans == 0xfe { break; }
-            if ntrans >= 0x80 { continue; }
+            if ntrans >= 0x80 { start += 1; continue; }
             channel += 1;
-            if ntrans == 0 { continue; }
-            // XXX check indices for overflow
-            byte += 1;
-            let nrecv = self.pif_ram[byte - 1];
-            byte += ntrans as usize;
-            let command = self.pif_ram[byte - 1];
-            // XXX check channel
-            if command == 0 {
-                if ntrans == 1 && nrecv == 3 {
-                    // type: controller, nothing plugged in
-                    self.pif_ram[byte]     = 0x05;
-                    self.pif_ram[byte + 1] = 0x0;
-                    self.pif_ram[byte + 2] = 0x02;
-                } else {
-                    self.pif_ram[byte - 2] |= 0x40;
-                }
-            } else if command == 1 {
-                if ntrans == 1 && nrecv == 4 {
-                    BigEndian::write_u32(&mut self.pif_ram[byte..], cstate);
-                } else {
-                    self.pif_ram[byte - 2] |= 0x40;
-                }
-            } else {
-                // error: invalid, marked in recv byte
-                self.pif_ram[byte - 2] |= 0x80;
+            if ntrans == 0 { start += 1; continue; }
+            let nrecv = self.pif_ram[start + 1] as usize;
+            let end = start + 2 + ntrans + nrecv;
+            if end > 63 {
+                // not enough bytes left to read and write
+                break;
             }
-            byte += nrecv as usize;
+            let cmdresult = {
+                let (input, output) = self.pif_ram[start + 2..end].split_at_mut(ntrans);
+                Si::do_command(channel, ntrans, nrecv, input, output, cstate)
+            };
+            if let Some(err) = cmdresult {
+                self.pif_ram[start + 1] |= err;
+            }
+            start = end;
         }
+        self.pif_ram[63] = 0;  // commands executed
+    }
+
+    fn do_command(channel: u8, ntrans: usize, nrecv: usize, input: &[u8],
+                  output: &mut [u8], cstate: u32) -> Option<u8> {
+        if channel != 1 {
+            return Some(0x80);  // nothing connected
+        }
+        match input[0] {  // command
+            0 => {  // read status
+                if !(ntrans == 1 && nrecv == 3) {
+                    return Some(0x40);
+                }
+                // type: controller, nothing plugged in
+                output[0] = 0x05;
+                output[1] = 0x0;
+                output[2] = 0x02;
+            }
+            1 => {  // read controller
+                if !(ntrans == 1 && nrecv == 4) {
+                    return Some(0x40);
+                }
+                BigEndian::write_u32(output, cstate);
+            },
+            _ => {
+                return Some(0x80);
+            }
+        }
+        None
     }
 }
 
